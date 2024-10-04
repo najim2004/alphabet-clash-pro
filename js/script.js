f007cc15d9dc00a2672c7d22a2763fcd00132971
najim2004 <najim10112004@gmail.com>
2024-10-04 16:37:12 +0600

    fixed error

--- js/script.js
+++ js/script.js
@@ -22,13 +22,17 @@ function removeBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
+
+const audio = new Audio();
 // -----------------------------------------------------------------------------
 
+let isGamePlay = false;
 
 function play() {
     hiddenElementById('home');
     showElementById('play-ground');
     continueGame()
+    isGamePlay = true
 }
 
 function continueGame() {
@@ -41,7 +45,9 @@ function continueGame() {
 document.addEventListener('keyup', handleKeyboardButtonPress)
 
 function handleKeyboardButtonPress(event) {
-    
+    if (isGamePlay === false) {
+        return
+    }
     const pressLatter = event.key;
 
     const currentLatter = document.getElementById('current-alphabet').innerText.toLowerCase();
@@ -58,15 +64,24 @@ function handleKeyboardButtonPress(event) {
         removeBackgroundColorById(currentLatter);
         points++;
         score.innerText = points;
+        audio.src = "../sounds/tada-fanfare-a-6313.mp3";
+        audio.play();
         continueGame();
+    }
     else {
         if (lifeLeft <= 1) {
             hiddenElementById('play-ground');
             showElementById('score')
             yourScore(points);
+            isGamePlay = false;
         }
         lifeLeft--;
+        const lifeLeftPercent = (lifeLeft / 4) * 100;
+        const artBoard = document.getElementById('artboard');
+        artBoard.style.background = `linear-gradient(#FFFFFFB3 ${lifeLeftPercent}%,red)`
         life.innerText = lifeLeft;
+        audio.src = "../sounds/wrong-answer.mp3";
+        audio.play();
 
     }
 }
@@ -75,7 +90,7 @@ function yourScore(points) {
     const getScore = document.getElementById('yourScore');
     getScore.innerText = points;
     document.getElementById('playAgain').addEventListener('click', function () {
-        
+
         const currentLatter = document.getElementById('current-alphabet').innerText.toLowerCase();
         removeBackgroundColorById(currentLatter);
 
@@ -83,5 +98,7 @@ function yourScore(points) {
         hiddenElementById('score');
         document.getElementById('points').innerText = 0;
         document.getElementById('life').innerText = 3;
+        const artBoard = document.getElementById('artboard');
+        artBoard.style.background = 'linear-gradient(#FFFFFFB3 100%,red)'
     })
 }
